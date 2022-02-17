71537507593de985ccbd84d47b2ab3beacf5a01e
FogDong <dongtianxin.tx@alibaba-inc.com>
2022-02-17 19:13:02 +0800

    fix ci
    Signed-off-by: FogDong <dongtianxin.tx@alibaba-inc.com>

--- test/e2e-test/hack/addon-vela-test.sh
+++ test/e2e-test/hack/addon-vela-test.sh
@@ -6,9 +6,13 @@ echo $ADDONS
 for i in $ADDONS ; do
     if [ $i == "observability" ]; then
       vela addon enable $i domain=abc.com
+      else if [ $i == "model-serving" ]; then
+        vela addon enable ./addons/$i serviceType=ClusterIP
       else
       vela addon enable ./addons/$i
     fi
+
+    kubectl get app addon-$i -n vela-system
 
     if [ $? -ne 0 ]; then
       echo -e "\033[31m addon $i cannot enable \033[0m"
