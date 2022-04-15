927df5776ad7e8c4f99cded10abf7e03f392afb2
楚岳 <wangyike.wyk@alibaba-inc.com>
2022-04-15 18:00:11 +0800

    pending test
    Signed-off-by: 楚岳 <wangyike.wyk@alibaba-inc.com>

--- test/e2e-test/hack/addon-vela-test.sh
+++ test/e2e-test/hack/addon-vela-test.sh
@@ -7,7 +7,7 @@ for i in $ADDONS ; do
     if [ $i == "observability" ]; then
       vela addon enable $i domain=abc.com || vela -n vela-system status addon-$i
       elif [ $i == "model-serving" ]; then
-      vela addon enable ./addons/$i serviceType=ClusterIP || vela -n vela-system status addon-$i
+      # vela addon enable ./addons/$i serviceType=ClusterIP || vela -n vela-system status addon-$i
       elif [ $i != "ocm-gateway-manager-addon" ] && [ $i != "terraform-baidu" ] && [ $i != "dex" ]; then
       vela addon enable ./addons/$i
     fi
