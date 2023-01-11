aac4a2d6c3dc6b981a855df062c44402341b12e8
wyike <wangyike.wyk@alibaba-inc.com>
2023-01-11 21:25:45 +0800

    fix checkSemVer (#596)
    Signed-off-by: wangyike.wyk <wangyike.wyk@alibaba-inc.com>
    
    Signed-off-by: wangyike.wyk <wangyike.wyk@alibaba-inc.com>

--- test/e2e-test/hack/check-semver.sh
+++ test/e2e-test/hack/check-semver.sh
@@ -2,6 +2,7 @@
 
 regex="v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?(-([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?(\+([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?"
 
+
 ADDONS=`ls -l addons | grep "^d" | awk '{print $9}' | sort`
 
 echo $ADDONS
@@ -9,6 +10,22 @@ echo $ADDONS
 for i in $ADDONS ; do
 
 version=$(cat addons/$i/metadata.yaml | grep "version"| awk '{print $2}')
+
+if [[ $version =~ $regex ]];then
+    echo "$i" "$version" is a semantic version
+    else
+      echo "$i" "$version" not a semantic version
+      exit 1
+fi;
+done
+
+EXPERIMENTAL_ADDONS=`ls -l experimental/addons | grep "^d" | awk '{print $9}' | sort`
+
+echo $EXPERIMENTAL_ADDONS
+
+for i in $EXPERIMENTAL_ADDONS ; do
+
+version=$(cat experimental/addons/$i/metadata.yaml | grep "version"| awk '{print $2}')
 
 if [[ $version =~ $regex ]];then
     echo "$i" "$version" is a semantic version
